25b57e0696b20c91ea3b24f2fc6553ab25a568f6
Marcos Kauan Santos Ribas <markauansr@gmail.com>
2025-09-18 02:29:41 -0300

    Criei a página principal

---
+++ site-exemplo/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Primeiro Site em Repositório!</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Olá, Mundo!</h1>";
+        echo "<hr>";
+        echo "<p>Este é o meu primeiro site em um repositório!.</p>";
+    ?>
+</body>
+</html>
