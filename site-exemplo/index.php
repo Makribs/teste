6155c707147c581e953bdf7497cb57071eb75816
Marcos Kauan Santos Ribas <markauansr@gmail.com>
2025-09-18 02:57:51 -0300

    Coloquei um formulário retroalimentado no index.php

--- site-exemplo/index.php
+++ site-exemplo/index.php
@@ -3,13 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../site-exemplo/template/style.css">
     <title>Meu Primeiro Site em Repositório!</title>
 </head>
 <body>
     <?php 
-        echo "<h1>Olá, Mundo!</h1>";
-        echo "<hr>";
-        echo "<p>Este é o meu primeiro site em um repositório!.</p>";
+        $firstName = $_GET['firstName'] ?? '';
+        $lastName = $_GET['lastName'] ?? '';
     ?>
+    <main>
+        <h1>Salutation!</h1>
+
+        <form action=<?=$_SERVER['PHP_SELF']?> method="get">
+            <label for="firstName">First Name:</label>
+            <input type="text" name="firstName" required>
+
+            <label for="lastName">Last Name:</label>
+            <input type="text" name="lastName" required>
+
+            <input type="submit" value="Send">
+        </form>
+
+        <?php if ($firstName && $lastName) {
+            echo "<br>
+                <section>
+                    <h2>Hello, $firstName $lastName! Nice to meet you! ^^</h2>
+                </section>";
+        }?>
+    </main>
 </body>
 </html>
